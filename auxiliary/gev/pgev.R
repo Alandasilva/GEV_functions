7e5d5277b7acd4372ce7692a577e78e014fd6a85
Alandasilva <alan.silva.6991@gmail.com>
2022-03-05 17:11:49 -0300

    Add pgev.R

--- auxiliary/gev/pgev.R
+++ auxiliary/gev/pgev.R
@@ -1,28 +1,26 @@
 ################################################################################
-### GEV - PDF
+### GEV - cDF
 ################################################################################
 
-dgev <- function(x,xi,mu,sigma,delta,log = FALSE){
+pgev <- function(x,xi,mu,sigma,delta,log = FALSE){
   if(sigma <= 0 || delta <= -1 || xi == 0){stop('the parameters must be sigma > 0, delta >-1 and xi != 0, respectively.')}
   t  <- function(x) {sigma*x*abs(x)^delta} # T(x) function
   #dt <- function(x) {sigma*(delta + 1)*abs(x)^delta} # T'(x) function
   tl <- function(x) { 1 + xi*(sigma*x*abs(x)^delta - mu) } # t(x) function
   
   if(xi > 0){
-    f <- sigma*(delta + 1)*(abs(x)^delta)*(tl(x))^(-1/xi - 1)*exp(-tl(x)^(-1/xi)) * ( sign(mu - 1/xi)*(abs(mu - 1/xi)/sigma)^(1/(delta + 1)) < x )
+    f <- exp(-tl(x)^(-1/xi)) * ( sign(mu - 1/xi)*(abs(mu - 1/xi)/sigma)^(1/(delta + 1)) < x )
   }
   if(xi < 0){
-    f <- sigma*(delta + 1)*(abs(x)^delta)*(tl(x)^(-1/xi - 1))*exp(-tl(x)^(-1/xi)) * ( sign(mu - 1/xi)*(abs(mu - 1/xi)/sigma)^(1/(delta + 1)) > x )
+    f <- exp(-tl(x)^(-1/xi)) * ( sign(mu - 1/xi)*(abs(mu - 1/xi)/sigma)^(1/(delta + 1)) > x )
   } 
   # else{
   #   f <- exp( -(t(x) - mu) - exp( -(t(x) - mu) ) )*dt(x)
   # }
-  if(is.nan(f)){f <- 0}
+  #if(is.nan(f)){f <- 0}
   if(log == TRUE){f <- log(f)}
   return(f)
 }
 
 mu = 0; sigma = 1; delta = 0; xi = 1
-dgev(x = -1,mu = 0, sigma = 1, delta = 0, xi = 1)
-
-curve(expr = dgev(x = x,mu = 0, sigma = 1, delta = 0, xi = 1), from = -4, to = 10)
+pgev(x = seq(-1,99,1),mu = 0, sigma = 1, delta = 1, xi = 1)
